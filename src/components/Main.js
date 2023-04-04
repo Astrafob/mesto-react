5a968f29111e267a3d6efbba690b7f5adaf06a6c
Oleg Vinogradov <astrafob@yandex.ru>
2023-04-04 19:19:02 +0300

    add a state from popups

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,23 @@
 import avatarProfile from '../image/profile-image.jpg';
 
-function Main() {
-
-  function handleEditAvatarClick() {
-    const popup = document.querySelector('#popupEditAvatarProfile');
-    popup.classList.add('popup_opened');
-  }
-
-  function handleEditProfileClick() {
-    const popup = document.querySelector('#popupEditProfile');
-    popup.classList.add('popup_opened');
-  }
-
-  function handleAddPlaceClick() {
-    const popup = document.querySelector('#popupAddCard');
-    popup.classList.add('popup_opened');
-  }
+function Main({onEditAvatar, onEditProfile, onAddPlace}) {
 
   return (
     <main className="main">
       <section className="profile">
         <div className="profile__info">
-          <button onClick={handleEditAvatarClick} className="profile__avatar-edit" type="button">
+          <button onClick={() => onEditAvatar(true)} className="profile__avatar-edit" type="button">
             <img src={avatarProfile} className="profile__photo" alt="фото профиля" />
           </button>
           <div className="profile__wrapper">
             <div className="profile__name">
               <h1 className="profile__title">Жак-Ив Кусто</h1>
-              <button onClick={handleEditProfileClick} className="button button_edit_open" aria-label="редактор данных пользователя" type="button"></button>
+              <button onClick={() => onEditProfile(true)} className="button button_edit_open" aria-label="редактор данных пользователя" type="button"></button>
             </div>
             <p className="profile__subtitle">Исследователь океана</p>
           </div>
         </div>
-        <button onClick={handleAddPlaceClick} className="button button_add_open" aria-label="добавление новых картинок" type="button"></button>
+        <button onClick={() => onAddPlace(true)} className="button button_add_open" aria-label="добавление новых картинок" type="button"></button>
       </section>
       <section className="cards">
       </section>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,62 +1,17 @@
 
-function PopupWithForm() {
+function PopupWithForm({ name, title, buttonText, children, isOpen, onClose }) {
   return (
     <>
-      <div className="popup" id="popupEditAvatarProfile">
+      <div className={`popup popup_type_${name} ${isOpen ? "popup_opened" : ""}`}>
         <div className="popup__container">
-          <form className="popup__edit-form" id="editAvatar" name="editAvatar" novalidate>
-            <h2 className="popup__title">Обновить аватар</h2>
-            <input className="popup__input-text" id="profilePhotoURL" name="link" type="url" placeholder="Ссылка на картинку"
-              required />
-            <span className="popup__input-error profilePhotoURL-error"></span>
-            <button className="popup__save-button" aria-label="сохранение данных" type="submit">Сохранить</button>
+          <form className="popup__edit-form" name={name} novalidate>
+            <h2 className="popup__title">{title}</h2>
+            {children}
+            <button className="popup__save-button" aria-label="сохранение данных" type="submit">{buttonText}</button>
           </form>
-          <button className="popup__close-button" id="closeEditAvatar" aria-label="закрыть попап" type="button"></button>
+          <button className="popup__close-button" aria-label="закрыть попап" type="button" onClick={onClose}></button>
         </div>
       </div>
-
-      <div className="popup" id="popupDeleteCard">
-        <div className="popup__container">
-          <form className="popup__edit-form" id="deleteCard" name="deleteCard">
-            <h2 className="popup__title">Вы уверены?</h2>
-            <button className="popup__save-button" aria-label="сохранение данных" type="submit">Да</button>
-          </form>
-          <button className="popup__close-button" id="closeDeleteCard" aria-label="закрыть попап" type="button"></button>
-        </div>
-      </div>
-
-      <div className="popup" id="popupEditProfile">
-        <div className="popup__container">
-          <form className="popup__edit-form" id="editProfile" name="editProfile" novalidate>
-            <h2 className="popup__title">Редактировать профиль</h2>
-            <input className="popup__input-text" id="name" name="name" type="text" maxlength="40" minlength="2"
-              placeholder="Имя" required />
-            <span className="popup__input-error name-error"></span>
-            <input className="popup__input-text" id="about" name="about" type="text" maxlength="200" minlength="2"
-              placeholder="Увлечение" required />
-            <span className="popup__input-error about-error"></span>
-            <button className="popup__save-button" aria-label="сохранение данных" type="submit">Сохранить</button>
-          </form>
-          <button className="popup__close-button" id="closeEditProfile" aria-label="закрыть попап" type="button"></button>
-        </div>
-      </div>
-
-      <div className="popup" id="popupAddCard">
-        <div className="popup__container">
-          <form className="popup__edit-form" id="editForm" name="editForm" novalidate>
-            <h2 className="popup__title">Новое место</h2>
-            <input className="popup__input-text" id="placeName" name="name" type="text" maxlength="30" minlength="2"
-              placeholder="Название" required />
-            <span className="popup__input-error placeName-error"></span>
-            <input className="popup__input-text" id="placePhotoURL" name="link" type="url" placeholder="Ссылка на картинку"
-              required />
-            <span className="popup__input-error placePhotoURL-error"></span>
-            <button className="popup__save-button" aria-label="сохранение данных" type="submit">Сохранить</button>
-          </form>
-          <button className="popup__close-button" id="closeAddCard" aria-label="закрыть попап" type="button"></button>
-        </div>
-      </div>
-
     </>
   );
 }
