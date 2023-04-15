52be58c4e2164255e5d02368cdcc942d9cbd3950
Oleg Vinogradov <astrafob@yandex.ru>
2023-04-15 19:57:19 +0300

    changes after review

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ function PopupWithForm({ name, title, buttonText, children, isOpen, onClose, onS
     <>
       <div className={`popup popup_type_${name} ${isOpen ? "popup_opened" : ""}`}>
         <div className="popup__container">
-          <form className="popup__edit-form" name={name} onSubmit={onSubmit} noValidate>
+          <form className="popup__edit-form" name={name} onSubmit={onSubmit}>
             <h2 className="popup__title">{title}</h2>
             {children}
             <button className="popup__save-button" aria-label="сохранение данных" type="submit">{buttonText}</button>
